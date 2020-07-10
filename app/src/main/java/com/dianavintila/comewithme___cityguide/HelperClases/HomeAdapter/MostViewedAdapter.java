0c3a2d51813a614e52a3d414fed5860b19af80b2
Vintila Diana - Georgiana <58642790+dianavintila@users.noreply.github.com>
2020-07-10 11:11:31 +0300

    first commit

---
+++ app/src/main/java/com/dianavintila/comewithme___cityguide/HelperClases/HomeAdapter/MostViewedAdapter.java
@@ -0,0 +1,61 @@
+package com.dianavintila.comewithme___cityguide.HelperClases.HomeAdapter;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.dianavintila.comewithme___cityguide.R;
+
+import java.util.ArrayList;
+
+public class MostViewedAdapter extends RecyclerView.Adapter<MostViewedAdapter.MostViewedViewHolder>  {
+    ArrayList<MostViewedHelperClass> mostViewedLocations;
+
+    public MostViewedAdapter(ArrayList<MostViewedHelperClass> mostViewedLocations) {
+        this.mostViewedLocations = mostViewedLocations;
+    }
+
+    @NonNull
+    @Override
+    public MostViewedViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.most_viewed_card_design, parent, false);
+        MostViewedViewHolder mostViewedViewHolder = new MostViewedViewHolder(view);
+        return mostViewedViewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MostViewedViewHolder holder, int position) {
+        MostViewedHelperClass mostViewedHelperClass=mostViewedLocations.get(position);
+
+        holder.image.setImageResource(mostViewedHelperClass.getImage());
+        holder.text.setText(mostViewedHelperClass.getTitle());
+        holder.desc.setText(mostViewedHelperClass.getDescription());
+
+    }
+
+
+    @Override
+    public int getItemCount() {
+        return mostViewedLocations.size();
+    }
+
+    public static class MostViewedViewHolder extends RecyclerView.ViewHolder {
+
+        ImageView image;
+        TextView text,desc;
+
+        public MostViewedViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            image = itemView.findViewById(R.id.mv_image);
+            text = itemView.findViewById(R.id.mv_title);
+            desc=itemView.findViewById(R.id.mv_desc);
+
+        }
+    }
+}
